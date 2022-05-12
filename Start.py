754fa9a64f844024117eac867f6b5c96d2a56e46
Riguene Hani <84547492+HaniRiguene@users.noreply.github.com>
2022-05-12 01:14:20 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -20,7 +20,7 @@ root.geometry('800x500')
 root.minsize(800,500)
 root.maxsize(800,500)
 
-root.title("CFIS- Criminal Face Identification System")
+root.title("Face Identification System")
 root.configure(bg="#382273")
 
 
@@ -36,9 +36,9 @@ photo=ImageTk.PhotoImage(image)
 photo_label=Label(image=photo,width=800,height=0,bg='white').place(x=0,y=0)
 photo_label
 
-label_0 = Label(root, text="Criminal Face Identification System",width=50,font=("bold", 20),anchor=CENTER,bg="#386184",fg="white")
+label_0 = Label(root, text="Face Identification System",width=50,font=("bold", 20),anchor=CENTER,bg="#386184",fg="white")
 label_0.place(x=0,y=100)
 
-Button(root, text='REGISTER CRIMINAL',width=35,height=3,bg='blue',fg='white',font=("bold", 11),command=register).place(x=250,y=180)
+Button(root, text='REGISTER Human',width=35,height=3,bg='blue',fg='white',font=("bold", 11),command=register).place(x=250,y=180)
 Button(root, text='PHOTO MATCH',width=35,height=3,bg='blue',fg='white',font=("bold", 11),command=detectCriminal).place(x=250,y=260)
 Button(root, text='VIDEO SURVEILLANCE',width=35,height=3,bg='red',fg='white',font=("bold", 11),command=VideoSurveillance).place(x=250,y=340)
